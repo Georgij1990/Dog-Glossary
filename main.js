fe0b1d81ea84ee3a1502e05d6f33fc8e2df35294
Georgij Kocsarjan <georgij.kocsarjan@gmail.com>
2024-01-06 17:09:40 +0100

    Add not-found paragraph

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 'use strict';
+
 const container = document.getElementById('content');
 const button = document.getElementById('button-random-dog');
-button.addEventListener('click', addRandomImage);
+let dogBreed;
+const inputField = document.getElementById('input-breed');
+const showBreedButton = document.getElementById('button-show-breed');
+
+
 async function addRandomImage() {
     try {
         const response = await fetch('https://dog.ceo/api/breeds/image/random');
@@ -16,7 +21,52 @@ async function addRandomImage() {
         } else {
             container.appendChild(image);
         }
-    } catch(error)  {
+    } catch (error) {
         console.log(error.errorMessage);
     }
 }
+
+async function showDog() {
+    try {
+        const url = 'https://dog.ceo/api/breed/' + dogBreed + '/images';
+        const response = await fetch(url);
+        const responseObject = await response.json();
+        const firstChild = container.firstChild;
+        const notFoundParagraph = document.getElementById('not-found');
+        if (responseObject.status === 'error') {
+            if (!notFoundParagraph) {
+                if (firstChild) {
+                    firstChild.remove();
+                }
+                const paragraph = document.createElement('p');
+                container.insertAdjacentHTML("beforebegin", "<p id='not-found'>Breed not found!</p>");
+                return;
+            } else {
+                return;
+            }
+        }
+        const imageURL = responseObject.message;
+        const image = document.createElement('img');
+        const random = imageURL.length > 1 ? Math.floor(Math.random() * imageURL.length) : 0;
+        image.src = imageURL[random];
+        if (firstChild) {
+            firstChild.remove();
+            container.appendChild(image);
+        } else {
+            if (notFoundParagraph) {
+                notFoundParagraph.remove();
+            }
+            container.appendChild(image);
+        }
+    } catch (error) {
+        console.log(error.errorMessage);
+    }
+}
+
+button.addEventListener('click', addRandomImage);
+
+inputField.addEventListener('change', (e) => {
+    dogBreed = e.target.value.toLowerCase();
+});
+
+showBreedButton.addEventListener('click', showDog);
